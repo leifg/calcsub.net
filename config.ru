a9de16db394d46e11b898e1663a5e9562f74cefa
Leif Gensert <leif@gensert.de>
2011-05-01 17:53:07 +0200

    added flattr information

--- config.ru
+++ config.ru
@@ -5,6 +5,4 @@ require "./calc.rb"
 require "./lib/settings.rb"
 
 enable :logging
-set :environment, :production
-set :port, 4567
 run Sinatra::Application
--- lib/settings.rb
+++ lib/settings.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 set :title, 'calcsub.net'
 set :jquery_version, '1.5.2'
-set :flattr, 'http://flattr.com'
+set :flattr, 'https://flattr.com/thing/179303/calcsub-net-subnet-calculator'
 set :dropbox, 'http://db.tt/x8UR7Yu'
 set :github, 'https://github.com/leifg/calcsub.net'
 set :ga_id, 'UA-23064407-1'
